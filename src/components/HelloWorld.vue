b3380276babdb0173234d5b656c2ab0eee05c637
Chris <ascendant2323@gmail.com>
2022-08-14 14:21:09 +0700

    First go

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,45 +1,148 @@
 <template>
   <div class="hello">
     <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-typescript" target="_blank" rel="noopener">typescript</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+    <div>
+      <input
+        type="text"
+        id="search"
+        placeholder="Type here..."
+        @input="parseInput"
+        v-model="searchTerm"
+      >
+    </div>
+    <br><br>
+    <div v-for="category in filteredCategoryList" :key="category.category">
+      <div class="category">{{category.category}}</div>
+      <div v-for="commandItem in category.items" :key="commandItem.name" class="command-item">
+        <div>{{commandItem.name}}</div>
+        <div>{{commandItem.command}}</div>
+      </div>
+    </div>
   </div>
 </template>
 
-<script lang="ts">
-import { defineComponent } from 'vue';
+<script setup lang="ts">
+import {onMounted, ref, computed} from 'vue'
+const msg = "foo"
+import axios from 'axios'
+const searchTerm = ref('')
+let xmlData = ref("Loading XML...")
+let jsonData = ref("")
+let objectData = ref({})
 
-export default defineComponent({
-  name: 'HelloWorld',
-  props: {
-    msg: String,
-  },
-});
+import convert from "xml-js"
+
+// type elementArray
+
+type attributes = {
+  name: string
+  value: string
+}
+
+type element = {
+  type: "element";
+  name: string
+  attributes: attributes
+  elements?: element[]
+}
+
+interface KeyCommandsFile {
+  elements: element[]
+}
+
+interface oItem {
+  name: string
+  command: string | null
+}
+
+interface oCategory {
+  category: string
+  items: oItem[]
+}
+
+
+function isElement(obj: element | object | false): obj is element {
+  if (!obj) return false
+   return "type" in obj && obj["type"] == "element";
+}
+
+function getElements(el: Array<element> | object | false): element[] | false {
+  if (!isElement(el)) return false
+  if ("elements" in el && el["elements"] !== undefined && el["elements"].length > 0) return el["elements"]
+  else return false
+}
+
+const defaultFilteredValue: oCategory[] = [
+  {category: "foo",
+  items: [
+    {
+      name: "bar",
+      command: "escape"
+    }
+  ]}
+]
+
+const categoryList: oCategory[] = []
+const filteredCategoryList = ref(defaultFilteredValue)
+
+function parseItem(el: element): oItem | false {
+  const elements = getElements(el)
+  if (!elements) return false
+  const name = elements[0]["attributes"]["value"]
+  let command: string | null = null
+  if (elements.length > 1) {
+    command = elements[1]["attributes"]["value"]
+  }
+  return {name, command}
+}
+
+function parseCommands(obj: KeyCommandsFile) {
+  const keyCommandsElements = obj["elements"][0]
+  if (isElement(keyCommandsElements)) {
+    const top = getElements(keyCommandsElements)
+    if (!top) return false
+    const categories = getElements(top[0])
+    if (!categories) return false
+
+    for (let i = 0; i < categories.length; i++) {
+      const items: oItem[] = [] 
+      const category = categories[i]
+      const categoryElements = getElements(category)
+      if (!categoryElements) return false
+      const categoryTitle = categoryElements[0]["attributes"]["value"]
+      categoryList.push()
+      const commandList = getElements(categoryElements[1])
+      if (!commandList) return false
+      for (let i = 0; i < commandList.length; i++) {
+        const command = commandList[i]
+        const oItem = parseItem(command)
+        if (oItem) items.push(oItem)
+      }
+      categoryList.push({category: categoryTitle, items})
+      filteredCategoryList.value = categoryList
+
+    }
+  }
+
+}
+
+function parseInput(input: string) {
+  console.log(searchTerm)
+}
+
+onMounted(() => {
+    axios.get('http://localhost:8080/Key Commands.xml')
+                  .then(response => {
+                      xmlData.value = response.data;
+                      console.log(xmlData)
+                      jsonData.value = convert.xml2json(xmlData.value, {compact: false, spaces: 4});
+                      objectData.value = JSON.parse(jsonData.value)
+                      parseCommands(objectData.value as unknown as KeyCommandsFile)
+
+    });
+  }
+)
+
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
